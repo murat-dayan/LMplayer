7b5c4bc918c90fb0c103a74e8ce12930ed7d28b2
muratdayan <muratdayan359@gmail.com>
2024-06-29 17:06:46 +0300

    viewbinding added for SongsFragment

--- app/src/main/java/com/muratdayan/lmplayer/presentation/songs/SongsFragment.kt
+++ app/src/main/java/com/muratdayan/lmplayer/presentation/songs/SongsFragment.kt
@@ -6,17 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.muratdayan.lmplayer.R
+import com.muratdayan.lmplayer.databinding.FragmentSongsBinding
 
 
 class SongsFragment : Fragment() {
 
+    private var _binding: FragmentSongsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_songs, container, false)
+    ): View{
+        _binding = FragmentSongsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
